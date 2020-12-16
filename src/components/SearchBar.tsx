1299ec40b3cea15f6c39a3914170a71294064687
philippe_mertens <mertensph@gmail.com>
2020-12-16 11:09:01 +0100

    Work on search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,20 +1,33 @@
+import { Feather } from "@expo/vector-icons";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 
 const SearchBar = () => {
   return (
     <View style={styles.container}>
-      <Text>Search Bar component</Text>
+      <Feather style={styles.icon} name="search" />
+      <TextInput style={styles.input} placeholder="Search" />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#F0EEEE",
     height: 50,
     borderRadius: 5,
-    marginHorizontal: 15,
-    backgroundColor: "#F0EEEE",
+    marginTop: 12,
+    marginHorizontal: 12,
+    flexDirection: "row",
+  },
+  icon: {
+    fontSize: 30,
+    alignSelf: "center",
+    marginHorizontal: 12,
+  },
+  input: {
+    flex: 1,
+    fontSize: 18,
   },
 });
 
