3b1a44f5ce523f61b845021f94d6715ce2a8c2c0
philippe_mertens <mertensph@gmail.com>
2021-01-31 10:47:37 +0100

    Cleaning

--- src/models/core/index.tsx
+++ src/models/core/index.tsx
@@ -3,6 +3,5 @@ export * from './contact.model';
 export * from './document.model';
 export * from './healthcareparty.model';
 export * from './patient.model';
-export * from './prop.model';
 export * from './service.model';
 export * from './user.model';
--- src/models/core/prop.model.tsx
+++
@@ -1,5 +0,0 @@
-import { NavigationContainerComponent } from 'react-navigation';
-
-export interface ReactFCProps extends React.FunctionComponent {
-  navigation: NavigationContainerComponent;
-}
